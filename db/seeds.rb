7779ccba5689860b8caad2352a587e1b7b1ba225
Nathan Tseng <nateguy@gmail.com>
2017-03-05 23:31:09 +0800

    ! seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Department.destroy_all
+Employee.destroy_all
+Tag.destroy_all
+
+Department.create([
+  { name: "Planets" },
+  { name: "Stars" },
+])
+
+Tag.create([
+  { name: "Jupiter" },
+  { name: "Mars" },
+  { name: "Alpha Centauri" },
+])
+
+employee_list = [
+  {
+    first_name: "Darth",
+    last_name: "Vader",
+    email: "darthvader@gmail.com",
+    department_id: Department.first.id,
+    tag_ids: [Tag.first.id],
+  },
+  {
+    first_name: "John",
+    last_name: "Bajorans",
+    email: "johnbaj@gmail.com",
+    department_id: Department.last.id,
+    tag_ids: [Tag.first.id, Tag.last.id],
+  }
+]
+
+employee_list.each do |employee|
+  Employee.create({
+    first_name: employee[:first_name],
+    last_name: employee[:last_name],
+    email: employee[:email],
+    department: Department.find(employee[:department_id]),
+    tag_ids: employee[:tag_ids],
+  })
+end
